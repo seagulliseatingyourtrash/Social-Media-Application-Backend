cddff70ee31ca24531876affdbfb9d8954fdc250
junthetechguy <forestwoodleaves@gmail.com>
2025-04-21 22:02:45 +0100

    build: Initial build.gradle setting

---
+++ src/test/java/com/example/socialmediaapplicationbackend/SocialMediaApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediaapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
