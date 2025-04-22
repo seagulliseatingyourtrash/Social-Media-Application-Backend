9ea3e7ec2f5ea9f04fb64c26223371fbfc657cce
junthetechguy <forestwoodleaves@gmail.com>
2025-04-22 21:52:21 +0100

    test: Created PostServiceTest and PostControllerTest

---
+++ src/test/java/com/example/socialmediaapplicationbackend/controller/PostControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.socialmediaapplicationbackend.controller;
+
+public class PostControllerTest {
+}
---
+++ src/test/java/com/example/socialmediaapplicationbackend/service/PostServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.socialmediaapplicationbackend.service;
+
+public class PostServiceTest {
+}
