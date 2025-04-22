40c53f84c029b60224a8a43f81aa9d2486440ceb
junthetechguy <forestwoodleaves@gmail.com>
2025-04-22 22:09:18 +0100

    test: Added post create test code for Post Service Test

--- src/main/java/com/example/socialmediaapplicationbackend/service/PostService.java
+++ src/main/java/com/example/socialmediaapplicationbackend/service/PostService.java
@@ -1,15 +1,27 @@
 package com.example.socialmediaapplicationbackend.service;
 
+import com.example.socialmediaapplicationbackend.exception.ErrorCode;
+import com.example.socialmediaapplicationbackend.exception.SimpleSnsApplicationException;
+import com.example.socialmediaapplicationbackend.model.entity.PostEntity;
+import com.example.socialmediaapplicationbackend.model.entity.UserEntity;
+import com.example.socialmediaapplicationbackend.repository.PostEntityRepository;
 import com.example.socialmediaapplicationbackend.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final UserEntityRepository userEntityRepository;
     private final PostEntityRepository postEntityRepository;
-    private final CommentEntityRepository commentEntityRepository;
-    private final LikeEntityRepository likeEntityRepository;
+
+    @Transactional
+    public void create(String userName, String title, String body) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName)
+                .orElseThrow(() -> new SimpleSnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("userName is %s", userName)));
+        PostEntity postEntity = PostEntity.of(title, body, userEntity);
+        postEntityRepository.save(postEntity);
+    }
 
 }
--- src/test/java/com/example/socialmediaapplicationbackend/service/PostServiceTest.java
+++ src/test/java/com/example/socialmediaapplicationbackend/service/PostServiceTest.java
@@ -1,10 +1,23 @@
 package com.example.socialmediaapplicationbackend.service;
 
+import com.example.socialmediaapplicationbackend.exception.ErrorCode;
+import com.example.socialmediaapplicationbackend.exception.SimpleSnsApplicationException;
+import com.example.socialmediaapplicationbackend.fixture.TestInfoFixture;
+import com.example.socialmediaapplicationbackend.fixture.UserEntityFixture;
+import com.example.socialmediaapplicationbackend.model.entity.PostEntity;
 import com.example.socialmediaapplicationbackend.repository.PostEntityRepository;
 import com.example.socialmediaapplicationbackend.repository.UserEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class PostServiceTest {
@@ -17,5 +30,26 @@ public class PostServiceTest {
 
     @MockBean
     PostEntityRepository postEntityRepository;
+
+    @Test
+    void should_create_post_successfully() {
+        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.of(UserEntityFixture.get(fixture.getUserName(), fixture.getPassword())));
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        Assertions.assertDoesNotThrow(() -> postService.create(fixture.getUserName(), fixture.getTitle(), fixture.getBody()));
+    }
+
+    @Test
+    void should_throw_when_user_not_found_on_create() {
+        TestInfoFixture.TestInfo fixture = TestInfoFixture.get();
+        when(userEntityRepository.findByUserName(fixture.getUserName())).thenReturn(Optional.empty());
+        when(postEntityRepository.save(any())).thenReturn(mock(PostEntity.class));
+
+        SimpleSnsApplicationException exception = Assertions.assertThrows(SimpleSnsApplicationException.class,
+                () -> postService.create(fixture.getUserName(), fixture.getTitle(), fixture.getBody()));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
 
 }
