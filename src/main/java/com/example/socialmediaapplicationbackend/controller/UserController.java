ab5bb01a9de83d96062a3c2a532d133bb89f313e
junthetechguy <forestwoodleaves@gmail.com>
2025-04-22 21:50:09 +0100

    feat: Added login function on UserController

--- src/main/java/com/example/socialmediaapplicationbackend/controller/UserController.java
+++ src/main/java/com/example/socialmediaapplicationbackend/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.example.socialmediaapplicationbackend.controller;
 
 import com.example.socialmediaapplicationbackend.controller.request.UserJoinRequest;
+import com.example.socialmediaapplicationbackend.controller.request.UserLoginRequest;
 import com.example.socialmediaapplicationbackend.controller.response.Response;
 import com.example.socialmediaapplicationbackend.controller.response.UserJoinResponse;
+import com.example.socialmediaapplicationbackend.controller.response.UserLoginResponse;
 import com.example.socialmediaapplicationbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,6 +22,17 @@ public class UserController {
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
         return Response.success(UserJoinResponse.fromUser(userService.join(request.getName(), request.getPassword())));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+        String token = userService.login(request.getName(), request.getPassword());
+        return Response.success(new UserLoginResponse(token));
+    }
+
+    @GetMapping("/me")
+    public Response<UserJoinResponse> me(Authentication authentication) {
+        return Response.success(UserJoinResponse.fromUser(userService.loadUserByUsername(authentication.getName())));
     }
 
 }
