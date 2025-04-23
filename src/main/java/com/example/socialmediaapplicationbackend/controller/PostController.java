eca0baa358d6469509d8e10d879f92cc300dd43c
junthetechguy <forestwoodleaves@gmail.com>
2025-04-23 23:14:54 +0100

    feat: Added create API handler on PostController

---
+++ src/main/java/com/example/socialmediaapplicationbackend/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.example.socialmediaapplicationbackend.controller;
+
+import com.example.socialmediaapplicationbackend.controller.request.PostWriteRequest;
+import com.example.socialmediaapplicationbackend.controller.response.Response;
+import com.example.socialmediaapplicationbackend.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@AllArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public Response<Void> create(@RequestBody PostWriteRequest request, Authentication authentication) {
+        postService.create(authentication.getName(), request.getTitle(), request.getBody());
+        return Response.success();
+    }
+
+}
