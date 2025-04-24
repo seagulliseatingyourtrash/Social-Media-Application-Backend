5140985f61f8157303d1c21e0fea933e869a6304
junthetechguy <forestwoodleaves@gmail.com>
2025-04-24 16:55:14 +0100

    feat: Added LikeEntityRepository

--- src/main/java/com/example/socialmediaapplicationbackend/repository/LikeEntityRepository.java
+++ src/main/java/com/example/socialmediaapplicationbackend/repository/LikeEntityRepository.java
@@ -1,8 +1,33 @@
 package com.example.socialmediaapplicationbackend.repository;
 
+import com.example.socialmediaapplicationbackend.model.entity.LikeEntity;
+import com.example.socialmediaapplicationbackend.model.entity.PostEntity;
+import com.example.socialmediaapplicationbackend.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface LikeEntityRepository extends JpaRepository {
+public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer> {
+    Optional<LikeEntity> findByUserAndPost(UserEntity user, PostEntity post);
+
+
+    @Query(value = "SELECT COUNT(*) from LikeEntity entity WHERE entity.post = :post")
+    Integer countByPost(@Param("post") PostEntity post);
+
+    List<LikeEntity> findAllByPost(PostEntity post);
+
+//    long countByPost(PostEntity post);
+
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE LikeEntity entity SET removed_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity post);
 }
