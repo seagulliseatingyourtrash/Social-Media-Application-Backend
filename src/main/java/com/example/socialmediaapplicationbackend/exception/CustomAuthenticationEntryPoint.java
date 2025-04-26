afc59dd8e856fab3e41276817dd459c958c638f3
junthetechguy <forestwoodleaves@gmail.com>
2025-04-26 14:30:42 +0100

    feat: Added Global Exception Handler for consistent error handling

--- src/main/java/com/example/socialmediaapplicationbackend/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/socialmediaapplicationbackend/exception/CustomAuthenticationEntryPoint.java
@@ -2,13 +2,14 @@ package com.example.socialmediaapplicationbackend.exception;
 
 import com.example.socialmediaapplicationbackend.controller.response.Response;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomAuthenticationEntryPoint {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setContentType("application/json");
